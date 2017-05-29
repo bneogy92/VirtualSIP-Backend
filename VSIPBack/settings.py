9629abc930c768205e9b50ab47cd3966e35d363a
Ankur Shukla <work.ankurshukla@gmail.com>
2017-05-29 17:09:11 +0530

    Begin wih imageio setup

--- VSIPBack/settings.py
+++ VSIPBack/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ImageIO.apps.ImageioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
